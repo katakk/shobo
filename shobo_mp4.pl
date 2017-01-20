b6b4e65684674c7a7ecb1f7426c4ff12f859a7a0
katakk <kata@katakuri.sakura.ne.jp>
2017-01-21 00:07:55 +0900

    use pgsql

--- shobo_mp4.pl
+++ shobo_mp4.pl
@@ -13,6 +13,85 @@ use Encode 'encode';
 use LWP::Simple qw/get/;
 use XML::Simple; #sudo yum install  expat-devel perl-XML-Simple
 use Data::Dumper;
+use DBI;
+use DBD::Pg;
+use DBD::SQLite;
+
+# PostgreSQL
+my $DSN="dbi:Pg:dbname=foltia;host=localhost;port=5432";
+#my $DSN="dbi:SQLite:syobocal.sqlite";
+my $DBUser="foltia";
+my $DBPass="foltiaadmin";
+my $dbh;
+  my $DBCreate =  <<"SQL";
+CREATE TABLE
+  syobocal_program
+    (tid bigint not null
+    , title text
+    , cat text
+    , firstyear text
+    , firstmonth text
+    , firstendyear text
+    , firstendmonth text
+    , userpoint text
+    , userpointRank text
+    , titleyomi text
+    , primary key (tid)
+);
+SQL
+
+  my $DBInsert1 =  <<"SQL";
+
+INSERT INTO
+  syobocal_program
+    (tid
+    , title
+    , cat
+    , userpoint
+    , userpointRank) VALUES (?,?,?,?,?)
+SQL
+
+  my $DBInsert2 =  <<"SQL";
+UPDATE syobocal_program 
+SET
+  titleyomi = ?
+WHERE
+  tid = ?
+SQL
+
+  my $DBInsert4 =  <<"SQL";
+UPDATE syobocal_program 
+SET
+  firstyear = ?,
+  firstmonth = ?
+WHERE
+  tid = ?
+SQL
+
+  my $DBInsert3 =  <<"SQL";
+UPDATE syobocal_program 
+SET
+  firstendyear = ?,
+  firstendmonth = ?
+WHERE
+  tid = ?
+SQL
+
+  my $DBQuery =  <<"SQL";
+  SELECT title
+    , cat
+    , firstyear
+    , firstmonth
+    , firstendyear
+    , firstendmonth
+    , userpoint
+    , userpointRank
+    , titleyomi 
+  FROM   syobocal_program
+  WHERE
+    tid = ?
+SQL
+
 setlogsock 'unix';
 openlog($0, 'pid', 'local0');
 my $regex = '\.(MP4)$';
@@ -54,9 +133,25 @@ sub tid_to_name
   my $number = shift;
   my $title;
   my $cat;
+  my $sth;
   return unless($number); #number=0 => TIDの指定が不正です
-  return if(exists($tid->{$number}));
-  sleep 1;
+  #PG>
+  $sth = $dbh->prepare($DBQuery);
+  $sth->execute($number);
+  ($tid->{$number}->{title},
+    $tid->{$number}->{cat},
+    $tid->{$number}->{FirstYear},
+    $tid->{$number}->{FirstMonth},
+    $tid->{$number}->{FirstEndYear},
+    $tid->{$number}->{FirstEndMonth},
+    $tid->{$number}->{UserPoint},
+    $tid->{$number}->{UserPointRank},
+    $tid->{$number}->{TitleYomi}) = $sth->fetchrow_array;
+  $sth->finish;
+
+  return if(defined($tid->{$number}->{title}));
+  #<PG
+ # sleep 1;
   my $config = XMLin(get(sprintf 
     qq|http://cal.syoboi.jp/db.php?Command=TitleLookup&TID=%d|,
      $number));
@@ -71,9 +166,10 @@ sub tid_to_name
 	$title =~ s/\>/＞/g if($title);
 	$title =~ s/\|/｜/g if($title);
 	$title =~ s/\"/”/g if($title);
-	$title =~ s/\!/！/g if($title); 
+	$title =~ s/\!/！/g if($title);
 	$title =~ s/\\/＼/g if($title);
-	$title =~ s/♡/_/g  if($title); 
+	$title =~ s/♡/_/g  if($title);
+	# local
 	$tid->{$number}->{title} = $title;
 	$tid->{$number}->{cat} = $config->{TitleItems}->{TitleItem}->{Cat};
 	$tid->{$number}->{FirstYear} = $config->{TitleItems}->{TitleItem}->{FirstYear};
@@ -83,7 +179,42 @@ sub tid_to_name
 	$tid->{$number}->{UserPoint} = $config->{TitleItems}->{TitleItem}->{UserPoint};
 	$tid->{$number}->{UserPointRank} = $config->{TitleItems}->{TitleItem}->{UserPointRank};
 	$tid->{$number}->{TitleYomi} = $config->{TitleItems}->{TitleItem}->{TitleYomi};
-syslog('info', "getting tid %d", $number);
+	#PG>
+	$tid->{$number}->{title} = encode('utf-8', $tid->{$number}->{title});
+	$tid->{$number}->{TitleYomi} = encode('utf-8', $tid->{$number}->{TitleYomi});
+	$sth = $dbh->prepare($DBInsert1);
+ 	$sth->execute($number,
+			$tid->{$number}->{title},
+			$tid->{$number}->{cat},
+			$tid->{$number}->{UserPoint},
+			$tid->{$number}->{UserPointRank},
+		);
+	$sth->finish;
+
+	$sth = $dbh->prepare($DBInsert2);
+	eval { $sth->execute(
+			$tid->{$number}->{TitleYomi},
+			$number
+		);};
+	$sth->finish;
+
+	$sth = $dbh->prepare($DBInsert4);
+	eval { $sth->execute(
+			$tid->{$number}->{FirstYear},
+			$tid->{$number}->{FirstMonth},
+			$number
+		);};
+	$sth->finish;
+
+	$sth = $dbh->prepare($DBInsert3);
+	eval { $sth->execute(
+			$tid->{$number}->{FirstEndYear},
+			$tid->{$number}->{FirstEndMonth},
+			$number
+		);};
+	$sth->finish;
+	#<PG
+eval { syslog('info', "getting tid %d", $number); } ;
 	return;
 }
 sub p{
@@ -181,11 +312,19 @@ sub d{
 }
 sub main
 {
-	eval { $tid = lock_retrieve($tiddb) if(-f $tiddb); };
-	if($@) { unlink $tiddb; };
+	#PG>
+	$dbh = DBI->connect($DSN,$DBUser,$DBPass,{RaiseError=>1}) || die $!;
+	eval {
+  		my $sth = $dbh->prepare($DBCreate);
+  		$sth->execute();
+  		$sth->finish;
+	};
+	#<PG
 	finddepth( { preprocess => \&p, wanted => \&t, nochdir => 0}, $dept );
 	finddepth( { preprocess => \&p, wanted => \&d, nochdir => 0}, $tv );
-	lock_nstore $tid, $tiddb;
+	#PG>
+	$dbh->disconnect;
+	#<PG
 }
 &main;
 
